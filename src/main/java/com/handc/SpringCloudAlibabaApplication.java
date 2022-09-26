492898db71c4e637f475104bf8d88bd1f8fed524
HuanChong <15289350682@163.com>
2022-09-26 10:09:04 +0800

    [chong.huan] chore: add dependency

---
+++ src/main/java/com/handc/SpringCloudAlibabaApplication.java
@@ -0,0 +1,13 @@
+package com.handc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAlibabaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/handc/SpringCloudAlibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
